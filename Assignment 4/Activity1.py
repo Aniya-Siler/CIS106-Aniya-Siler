40382f4216e7ce7139d7acd4131b4924f34dda57
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-09-14 15:48:01 -0500

    Create Activity1.py

---
+++ Assignment 4/Activity1.py
@@ -0,0 +1,18 @@
+# This program prompts user for hours worked and calculates the rate per hour, weekly, monthly, and annual (gross) pay
+
+print("Enter hours worked:")
+hours = float(input())
+
+print("Enter rate per hr:")
+rate = float(inout())
+
+weekly = (hours*rate)
+
+monthly = (weekly*52) /12
+
+annual = (weekly*52)
+
+print("Weekly income is") + str(weekly)
+print("Monthly income is") + str(monthly)
+print("Annual income is") + str(annual)
+# Refences I used were the Programming Fundamentals book (Data and Operations) I also used google to search up the specific calculations.
