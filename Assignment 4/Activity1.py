246a62d0080dc13254fcea4241e1f2308c25d2af
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-09-14 20:39:18 -0500

    Update Activity1.py

--- Assignment 4/Activity1.py
+++ Assignment 4/Activity1.py
@@ -12,8 +12,8 @@ monthly = (weekly*52)/12
 
 annual = (weekly*52)
 
-print("Weekly income is" + str(weekly)
-print("Monthly income is" + str(monthly)
-print("Annual income is" + str(annual)
+print("Weekly income is" + str(weekly))
+print("Monthly income is" + str(monthly))
+print("Annual income is" + str(annual))
       
 # Refences I used were the Programming Fundamentals book (Data and Operations) I also used google to search up the specific calculations.
