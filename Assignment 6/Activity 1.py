1d7dd3a194ab8c736cbf4c156584ce6384eca26b
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-09-25 16:29:12 -0500

    Create Activity 1.py

---
+++ Assignment 6/Activity 1.py
@@ -0,0 +1,6 @@
+This program calculates the rate per hour, weekly, monthly, & annual pay.
+
+# References:
+#   https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-3/
+
+def get_()
