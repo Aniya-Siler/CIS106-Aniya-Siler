75913f30afdfb7bbfd7cd61216e729d8b4b7aa48
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-09-27 13:36:12 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -12,35 +12,35 @@ def get_hours():
 
                 
 def get_rate():
-	print("Enter rate per hr:")
-	rate = float(input())
-	return rate
+        print("Enter rate per hr:")
+        rate = float(input())
+        return rate
 
                
 def calculate_weekly(rate, hours):
-	weekly = (rate * hours)
-	return weekly
+        weekly = (rate * hours)
+        return weekly
 
                
 def calculate_monthly(weekly):
-	monthly = (Weekly * 52 / 12)
-	return annual
+        monthly = (Weekly * 52 / 12)
+        return annual
 
 
 def calculate_annual(weekly):
-	annual = (weekly * 52)
-	return annual
+        annual = (weekly * 52)
+        return annual
 
                
 def display_result(weekly, monthly, annual):
-	print(str("Weekly income is " & weekly))
-	print(str("Monthly income is " & monthly))
-	print(str("Annual income is " & annual))
+        print(str("Weekly income is " & weekly))
+        print(str("Monthly income is " & monthly))
+        print(str("Annual income is " & annual))
 	
  
 def main():
-	hours = get_hours()
-	rate = get_rate()
-	display_result(weekly, monthly, annual)
+        hours = get_hours()
+        rate = get_rate()
+        display_result(weekly, monthly, annual)
 	
-	main()
+        main()
