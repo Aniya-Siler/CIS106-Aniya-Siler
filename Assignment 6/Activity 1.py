463dc93678c7cc647012ee9b7883df173603390f
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-09-27 14:04:39 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -6,40 +6,40 @@
 # https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-3/
 
 def get_hours():
-        print("Enter amount of hours worked:")
-        hours = float(input())
-        return hours
+    print("Enter amount of hours worked:")
+    hours = float(input())
+    return hours
         
         
 def get_rate():
-         print("Enter rate per hour:")
-         rate = float(input())
-         return hours
+     print("Enter rate per hour:")
+     rate = float(input())
+     return hours
          
          
 def calculate_weekly(rate, hours):
-         weekly = (rate * hours)
-         return weekly
+     weekly = (rate * hours)
+     return weekly
          
 def calculate_monthly(weekly):
-        annual = (weekly * 52 / 12)
-        return monthly
+    annual = (weekly * 52 / 12)
+    return monthly
         
         
 def calculate_annual(weekly):
-         annual = (weekly * 52)
-         return annual
+     annual = (weekly * 52)
+     return annual
          
          
 def display_result(weekly, monthly, annual):
-          print(str("Weekly income is " & weekly))
-          print(str("Monthly income is " & monthly))
-          print(str("Annual income is " & annual))
+      print(str("Weekly income is " & weekly))
+      print(str("Monthly income is " & monthly))
+      print(str("Annual income is " & annual))
           
           
 def main():
-           hours = get_hours()
-           rate = get_rate()
-           display_result(weekly, monthly, annual)
+       hours = get_hours()
+       rate = get_rate()
+       display_result(weekly, monthly, annual)
            
 main()
