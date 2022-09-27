d52626431e7237bef2a1fbd1edfb12826bb62a71
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-09-26 20:49:54 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -1,6 +1,18 @@
-This program calculates the rate per hour, weekly, monthly, & annual pay.
-
+# This program asks the user for amount of hours,
+# Calculates the useres weekly, monthly, and annual pay,
+# and displats the results.
+#
 # References:
-#   https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-3/
+# https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-3/
 
-def get_()
+def get_hours():
+  print("Enter amount hours worked:")
+  hours = float(input()
+  return hours
+                
+def get_rate():
+	print("Enter rate per hr:")
+  rate = float(input()
+  return rate
+               
+def calculate_weekly
