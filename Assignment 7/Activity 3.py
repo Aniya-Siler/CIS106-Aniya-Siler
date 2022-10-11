15adf3bce402fcce8a4561c914bcd7f6a8e052d5
Dave Braunschweig <dave@itrain.ms>
2022-10-11 18:59:05 -0500

    Assignment 7 corrections

--- Assignment 7/Activity 3.py
+++ Assignment 7/Activity 3.py
@@ -1,3 +1,8 @@
+def calculateCenti(miles):
+    centi = miles * 160934
+    
+    return centi
+
 def calculateFeet(miles):
     feet = miles * 5280.019685
     
@@ -8,15 +13,35 @@ def calculateInches(miles):
     
     return inches
 
-def calculateYard(miles):
-    yard = miles * 1760.0065617
+def calculateKilo(miles):
+    kilo = miles * 1.60934
     
-    return yard
+    return kilo
 
-def displayResult(miles, yard, feet, inches):
-    print("The Distance in Yards is" + str(yard) + "yds")
-    print("The Distance in Feet is" + str(feet) + "ft")
-    print("The Distance in Inches is" + str(inches) + "Ins")
+def calculateMeters(miles):
+    meters = miles * 1609.34
+    
+    return meters
+
+def calculateYards(miles):
+    yards = miles * 1760.0065617
+    
+    return yards
+
+def displayResult(result, label):
+    print(str(result) + " " + label)
+
+def getChoice():
+    print("Enter US to convert to US or Metric to convert to Metric:")
+    choice = input()
+    
+    return choice
+
+def getMeasurment(scale):
+    print("Enter" + scale + "Measurment:")
+    measurment = float(input())
+    
+    return measurment
 
 def getMiles():
     print("Enter amount of Miles.")
@@ -24,12 +49,34 @@ def getMiles():
     
     return miles
 
+def processMetric(miles):
+    result = calculateKilo(miles)
+    displayResult(result, "Kilometers")
+    result = calculateMeters(miles)
+    displayResult(result, "Meters")
+    result = calculateCenti(miles)
+    displayResult(result, "Centimeters")
+
+def processUS(miles):
+    result = calculateYards(miles)
+    displayResult(result, "Yards")
+    result = calculateFeet(miles)
+    displayResult(result, "Feet")
+    result = calculateInches(miles)
+    displayResult(result, "Inches")
+
 # Main
-# This program asks the user for amount of Miles,
-# Converts the given amount to Yards, Feet, Inches,
-# and displays the results.
+# Ask the user to input distance in miles,
+# and ask if they would like to convert the distance to US or Metric.
+# Display Results.
+# References:
+# Go here ...
 miles = getMiles()
-yard = calculateYard(miles)
-feet = calculateFeet(miles)
-inches = calculateInches(miles)
-displayResult(miles, yard, feet, inches)
+choice = getChoice()
+if choice == "Metric" or choice == "metric":
+    processMetric(miles)
+else:
+    if choice == "US" or choice == "us":
+        processUS(miles)
+    else:
+        print("You must enter Metric to convert to Metric or US to convert to US!")
