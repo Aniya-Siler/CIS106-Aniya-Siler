3e1238a4135580220f8146d7866fcaabe9ba0707
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-10-17 12:18:41 -0500

    Create Activity 1.py

---
+++ Assignment 8/Activity 1.py
@@ -0,0 +1,23 @@
+def displayExpression(value, count, result):
+    print(str(value) + " * " + str(count) + " = " + str(result))
+
+def getValue(name):
+    print("Enter " + name + ":")
+    value = int(input())
+    
+    return value
+
+def processExpressions(value, expressioncount):
+    for count in range(1, expressioncount + 1, 1):
+        result = value * count
+        displayExpression(value, count, result)
+
+# Main
+# This program uses a loop to generate a list of
+# mulitplication expressions for a given value
+# Refrences:
+# https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/loop-examples/
+# https://www.youtube.com/watch?v=yaqMSBr_NCU&t=347s
+value = getValue("value")
+expressioncount = getValue("expression")
+processExpressions(value, expressioncount)
