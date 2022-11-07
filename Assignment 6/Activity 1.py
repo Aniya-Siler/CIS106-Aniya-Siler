97e39be1bc44c3ace7025e09078d38db31810506
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-11-07 15:03:44 -0600

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -1,50 +1,58 @@
-# This program asks the user for amount of hours,
-# Calculates the useres weekly, monthly, and annual pay,
-# and displats the results.
-#
-# References:
-# https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-3/
+# This program asks the user to enter their birthday and then displays the day of the week they were born
+#Refrences:
+# https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/displaying-array-members/
+# https://www.geeksforgeeks.org/zellers-congruence-find-day-date/
 
-def get_hours():
-    print("Enter amount of hours worked:")
-    hours = float(input())
-    return hours
-        
-        
-def get_rate():
-    print("Enter rate per hour:")
-    rate = float(input())
-    return rate
-         
-         
-def calculate_weekly(rate, hours):
-    weekly = (rate * hours)
-    return weekly
-   
-    
-def calculate_monthly(weekly):
-    monthly = (weekly * 52 / 12)
-    return monthly
-        
-        
-def calculate_annual(weekly):
-    annual = (weekly * 52)
-    return annual
-         
-         
-def display_result(weekly, monthly, annual):
-    print("Weekly income is " + str(weekly))
-    print("Monthly income is " + str(monthly))
-    print("Annual income is " + str(annual))
-          
-          
-def main():
-    hours = get_hours()
-    rate = get_rate()
-    weekly = calculate_weekly(hours, rate)
-    monthly = calculate_monthly(weekly)
-    annual = calculate_annual(weekly)
-    display_result(weekly, monthly, annual)
+def get_year():
+    print("What year were you born?")
+    year = int(input())
+    return year
+
+def get_month():
+    print("What month were you born?")
+    month = int(input())
+    return month
+
+def get_day():
+    print("What day were you born?")
+    day = int(input())
+    return day
+
+def calculate_zellers_congruence(year,month,day):
+    if (month == 1) :
+        month = 13
+        year = year - 1
  
+    if (month == 2) :
+        month = 14
+        year = year - 1
+    q = day
+    m = month
+    k = year % 100;
+    j = year // 100;
+    h = q + 13 * (m + 1) // 5 + k + k // 4 + j // 4 + 5 * j
+    h = h % 7
+    return h
 
+def lookdayname(day_number):
+    days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    day_name = days[day_number]
+    return day_name
+    
+def display_results(day_name):
+    print("Your day of birth landed on a " + str(day_name))
+def main():
+    year = get_year()
+    print(year)
+    month = get_month()
+    print(month)
+    day = get_day()
+    print(day)
+    zellers_congruence = calculate_zellers_congruence(year,month,day)
+    print(zellers_congruence)
+    day_name = lookdayname(zellers_congruence)
+    print(day_name)
+    display_results(day_name)
+    
+    
 main()
