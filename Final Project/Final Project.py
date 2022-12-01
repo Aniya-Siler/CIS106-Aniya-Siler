a5a68bb9f18245cd8c97ed3bb248f0d9f277f335
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-12-01 12:31:56 -0600

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -1,1 +1,26 @@
+# This program reads from a CD Catalog and Builds arrays
+# Then displays the results as title, artist, country, price, year
+# References:
+#
+
+
+def get_title(filename):
+    filename = open("Catalog.txt","r")
+    title = []
+    line1 = filename.readline()
+    line1 = filename.readline()
+    while True:
+        line1 = filename.readline()
+        if line1== "":
+            break
+        else:
+            line1 = line1.strip()
+def main():
+    filename = 'Catalog.txt'
+    title = get_title(filename)
+    
+main()        
+        
+        
+        
 
