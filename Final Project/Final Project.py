d9010b2c26dfa1f162fb587bb4a0a4860dc49bf7
Aniya-Siler <111772456+Aniya-Siler@users.noreply.github.com>
2022-12-15 13:34:54 -0600

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -8,7 +8,7 @@
 def get_common(filename):
     try:
         filename = open("plant_catalog.xml", "r")
-        c = []
+        commons = []
         while True:
             line1 = filename.readline()
             if line1== "" :
@@ -17,16 +17,16 @@ def get_common(filename):
                 line1 = line1.strip()
                 line1 = line1.replace('<COMMON>', '')
                 line1 = line1.replace('</COMMON>', '')
-                c.append(line1)
+                commons.append(line1)
     except Exception as e:
         print(e)
-    return c
+    return commons
 
 
 def get_botanical(filename):
     try:
         filename = open('plant_catalog.xml', 'r')
-        b = []
+        botanicals = []
         while True:
             line2 = filename.readline()
             if line2== "" :
@@ -36,16 +36,16 @@ def get_botanical(filename):
                 line2 = line2.strip()
                 line2 = line2.replace('<BOTANICAL>', '')
                 line2 = line2.replace('</BOTANICAL>', '')
-                b.append(line2)
+                botanicals.append(line2)
     except Exception as e:
         print(e)
-    return b
+    return botanicals
 
 
 def get_zone(filename):
     try:
         filename = open('plant_catalog.xml', 'r')
-        z = []
+        zones = []
         while True:
             line3 = filename.readline( )
             if line3== "":
@@ -55,16 +55,16 @@ def get_zone(filename):
                 line3 = line3.strip()
                 line3 = line3.replace('<ZONE>', '')
                 line3 = line3.replace('</ZONE>', '')
-                z.append(line3)
+                zones.append(line3)
     except Exception as e:
         print(e)
-    return z
+    return zones
 
 
 def get_price(filename):
     try:
         filename = open('plant_catalog.xml', 'r')
-        p = []
+        prices = []
         while True:
             line4 = filename.readline( )
             if line4== "":
@@ -75,63 +75,59 @@ def get_price(filename):
                 line4 = line4.replace('<PRICE>', '')
                 line4 = line4.replace('</PRICE>', '')
                 line4 = line4.replace('$', '')
-                p.append(float(line4))
+                prices.append(float(line4))
     except Exception as e:
-        print('Error: Missing or bad data')
-    return p
+        print(e)
+    return prices
 
 
 def get_light(filename):
     try:
         filename = open('plant_catalog.xml', 'r')
-        l = []
+        lights = []
         while True:
             line5 = filename.readline()
             if line5== "":
                 break
             
             if "<LIGHT>" in line5:
-                line5 = line5.strip( )
+                line5 = line5.strip()
                 line5 = line5.replace('<LIGHT>', '')
                 line5 = line5.replace('</LIGHT>', '')
-                l.append(line5)
+                lights.append(line5)
     except Exception as e:
         print(e)
-    return l
+    return lights
 
 
-def get_a(p):
-    total = 36
-    for index in range(len(p)):
-        total += p[index]
-    a = total /len(p)
-    return a
+def get_average(prices):
+    total = 0
+    for index in range(len(prices)):
+        total += prices[index]
+    average = total /len(prices)
+    return average
 
+def get_items(commons):
+    items = len(commons)
+    return items
 
-def get_i(c):
-    i = len(c)
-    return i
-
-
-def display_results(c, b, z, l, p, a, i):
+def display_results(commons, botanicals, zones, lights, prices, average, items):
     for j in range(36):
-        print(c[j] + '(' + b[j] + ')' + ' - ' + z[j] + ' - ' + l[j] + ' - ' + '$' + str(p[j]))
-    print(str(i) + ' items' + ' - ' + '$' + str(a))
+        print(commons[j] + '(' + botanicals[j] + ')' + ' - ' + zones[j] + ' - ' + lights[j] + ' - ' + '$' + str(prices[j]))
+    print(str(items) + ' items' + ' - ' + '$' + str(average))
         
         
 def main():
     filename = 'plant_catalog.xml'
-    c = get_common(filename)
-    b = get_botanical(filename)
-    z = get_zone(filename)
-    p = get_price(filename)
-    l = get_light(filename)
-    a = get_a(p)
-    i = get_i(c)
-    display_results(c, b, z, l, p, a, i)
+    commons = get_common(filename)
+    botanicals = get_botanical(filename)
+    zones = get_zone(filename)
+    prices = get_price(filename)
+    lights = get_light(filename)
+    average = get_average(prices)
+    items = get_items(commons)
+    display_results(commons, botanicals, zones, lights, prices, average, items)
     
     
 main()        
-        
-        
         
